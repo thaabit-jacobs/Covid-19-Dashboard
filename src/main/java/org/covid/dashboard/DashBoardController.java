fd9cbe4d1f6ce52af547d8b543c8b54811e0fd31
thaabit <mohammedthaabitjacobs@gmail.com>
2021-05-28 18:31:21 +0200

    bootstrap

--- src/main/java/org/covid/dashboard/DashBoardController.java
+++ src/main/java/org/covid/dashboard/DashBoardController.java
@@ -17,12 +17,12 @@ public class DashBoardController {
     public String home(Model model){
         model.addAttribute("total", virusDataStatsService.getGlobalTotalConfirmedCases());
         model.addAttribute("death", virusDataStatsService.getGlobalTotalDeathCases());
-/*        model.addAttribute("recover", virusDataStatsService.getGlobalTotalRecoveriesCases());
+        model.addAttribute("recover", virusDataStatsService.getGlobalTotalRecoveriesCases());
         model.addAttribute("confirmedCases", virusDataStatsService.getCountryTotalConfirmedCases("South Africa"));
         model.addAttribute("deathCases", virusDataStatsService.getCountryTotalDeathCases("South Africa"));
         model.addAttribute("recoveredCases", virusDataStatsService.getCountryTotalRecoverCases("South Africa"));
         model.addAttribute("vacc", virusDataStatsService.getGlobalTotalVaccinatedCases());
-        model.addAttribute("vaccCo", virusDataStatsService.getCountryTotalVaccinatedCases("South Africa"));*/
+        model.addAttribute("vaccCo", virusDataStatsService.getCountryTotalVaccinatedCases("South Africa"));
         return "index";
     }
 }
--- src/main/java/org/covid/dashboard/servcies/VirusDataStatsService.java
+++ src/main/java/org/covid/dashboard/servcies/VirusDataStatsService.java
@@ -1,13 +1,7 @@
 package org.covid.dashboard.servcies;
 
-import org.covid.dashboard.model.Cases;
-import org.covid.dashboard.model.CountryCaseData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class VirusDataStatsService {
@@ -15,48 +9,17 @@ public class VirusDataStatsService {
     @Autowired
     private VirusDataFetcherService virusDataFetcherService;
 
-    private List<CountryCaseData> countryCaseDataList = new ArrayList<>();
-
-    @PostConstruct
-    public void populateCountryCaseDataList(){
-        virusDataFetcherService.getConfirmedCasesList().stream()
-        .forEach(c -> {
-            CountryCaseData ccd = new CountryCaseData();
-            ccd.setCountry(c.getCountry());
-            ccd.setState(c.getState());
-            ccd.getConfirmedCasesMap().put("currentDaysCases",c.getCurrentDaysCases());
-            ccd.getConfirmedCasesMap().put("previousDaysCases",c.getPreviousDaysCases());
-            countryCaseDataList.add(ccd);
-        });
-
-        for (Cases c: virusDataFetcherService.getDeathCasesList()) {
-            for (CountryCaseData ccd: countryCaseDataList) {
-                ccd.getDeathCasesMap().put("currentDaysCases",c.getCurrentDaysCases());
-                ccd.getDeathCasesMap().put("previousDaysCases",c.getPreviousDaysCases());
-                break;
-            }
-        }
-
-        for (Cases c: virusDataFetcherService.getRecoveredCasesList()) {
-            for (CountryCaseData ccd: countryCaseDataList) {
-                ccd.getRecoveredCasesMap().put("currentDaysCases",c.getCurrentDaysCases());
-                ccd.getRecoveredCasesMap().put("previousDaysCases",c.getPreviousDaysCases());
-                break;
-            }
-        }
-    }
-
     public long getGlobalTotalConfirmedCases(){
-        return countryCaseDataList.stream()
-                .map((ccd) -> ccd.getConfirmedCasesMap().get("currentDaysCases"))
+        return virusDataFetcherService.getConfirmedCasesList().stream()
+                .map((c) -> c.getCurrentDaysCases())
                 .reduce((a, b) -> a + b)
                 .get();
     }
 
     public long getGlobalTotalDeathCases(){
-        return countryCaseDataList.stream()
-                .map((ccd) -> ccd.getConfirmedCasesMap().get("currentDaysCases"))
-                .reduce((a, b) -> a + b)
+        return virusDataFetcherService.getDeathCasesList().stream()
+                .map((c) -> c.getCurrentDaysCases())
+                .reduce((a, b) -> a +b)
                 .get();
     }
 
